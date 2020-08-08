91c9acd81e7c11187e0e43b60ca2cb2666622f8d
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-08 08:41:34 -0300

    Refatoracao de cófigo

--- MyMoneyApi/MyMoneyApi/Controllers/TipoMovimentacaoController.cs
+++ MyMoneyApi/MyMoneyApi/Controllers/TipoMovimentacaoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyMoneyApi.Models;
 using MyMoneyApi.Repositorio;
@@ -28,6 +29,7 @@ namespace MyMoneyApi.Controllers
 
         // GET api/<TipoMovimentacaoController>/5
         [HttpGet("{id}", Name = "GetTipoMovimentacao")]
+        [Authorize]
         public IActionResult GetById(int id)
         {
             var tipoMovimentacao = _repo.Find(id);
